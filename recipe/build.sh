6e131da4c524631af38dd7c67c252702ea4f134b
Isuru Fernando <isuruf@gmail.com>
2019-01-28 17:34:46 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,3 +3,5 @@
 source activate "${CONDA_DEFAULT_ENV}"
 
 python setup.py install --symengine-dir=$PREFIX --single-version-externally-managed --record record.txt
+
+python -c $'import symengine;\nif not symengine.test():\n raise Exception("Tests failed")'
