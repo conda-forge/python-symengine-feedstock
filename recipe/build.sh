b0d158f453c92d84bfcf400968fa5d34fb12825f
Isuru Fernando <isuruf@gmail.com>
2021-03-14 10:27:53 -0500

    pass cmake args

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-python setup.py install --symengine-dir=$PREFIX --single-version-externally-managed --record record.txt
+PYTHON_ARGS="-D IGNORE_THIS=1"
+for ARG in $CMAKE_ARGS; do
+  if [[ "$ARG" == "-DCMAKE_"* ]] && [[ "$ARG" != *";"* ]]; then
+    cmake_arg=$(echo $ARG | cut -d= -f1)
+    cmake_arg=$(echo $cmake_arg| cut -dD -f2-)
+    cmake_val=$(echo $ARG | cut -d= -f2-)
+    PYTHON_ARGS="$PYTHON_ARGS;${cmake_arg}=${cmake_val}"
+  fi
+done
+
+python setup.py build_ext --symengine-dir=$PREFIX $PYTHON_ARGS install --single-version-externally-managed --record record.txt
