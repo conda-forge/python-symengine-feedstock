8ba21288c4c394e1abbaffabc61e8355bdfa1f86
Isuru Fernando <isuruf@gmail.com>
2022-02-19 01:25:13 -0600

    fix line continuation char

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
-pip install . \
-  --global-option="build_ext" \
-  --global-option="--generator=Ninja" \
-  --global-option="install" \
-  --global-option="--symengine-dir=%LIBRARY_PREFIX%" \
+pip install . ^
+  --global-option="build_ext" ^
+  --global-option="--generator=Ninja" ^
+  --global-option="install" ^
+  --global-option="--symengine-dir=%LIBRARY_PREFIX%" ^
   -vv
   
